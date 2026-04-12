29f47ece51eb8f18dd40076def94ace55e3e4f73
K1CE <6kouman@gmail.com>
2026-04-11 23:31:21 -0700

    feat: make metal transform

--- DangerMageVS/Spells/SpellMetal.cs
+++ DangerMageVS/Spells/SpellMetal.cs
@@ -48,18 +48,70 @@ namespace SFDScript
 				particleExplosion("S_P", sender.position, 3, 8f);
             }
 			public const float PISTOL_DAMAGE = 3.33f;
+			private const float chunkCloseness = 2.4f;
             public override void explode(Cast sender, IObject alreadyHit, Vector2 position) {
 				if (hitPlayer) return;
-				float bullets = splash * 1.5f;
-				for (int i = 0; i < bullets; i++) {
-					double rotation = rnd.NextDouble() * Math.PI * 2;
-					Vector2 vector = new Vector2((float)Math.Cos(rotation) / 10f, (float)Math.Sin(rotation) / 10f);
-					vector.Normalize();
-                    IProjectile shrapnel = Game.SpawnProjectile(ProjectileItem.PISTOL, position + vector * 10f, vector);
-					shrapnel.DamageDealtModifier = (spellPower/10f)/PISTOL_DAMAGE;
-					shrapnel.CritChanceDealtModifier = 0f;
-					//shrapnel.Velocity = vector;
+				bool fat = spellPower > 6 || splash > 12;
+				IObject[] chunks;
+				if (sawblade != null) {
+					sawblade.Destroy();
+				}
+
+                Game.PlaySound("MeleeBlockMetal", position, 1f);
+                if (fat)
+				{
+					chunks = new IObject[3];
+					float rotation = (float)(rnd.NextDouble() * Math.PI * 2);
+					for(int i = 0; i < 3; i++)
+					{
+						float localRotation = (float)(rotation + (i * Math.PI * 2 / 3));
+						float facingRotation = (float)(Math.PI / 2 + localRotation);
+						Vector2 relativePos = new Vector2((float)Math.Cos(localRotation) * chunkCloseness, (float)Math.Sin(localRotation) * chunkCloseness);
+						chunks[i] = Game.CreateObject("MetalDebris00" + (char)(rnd.Next(3) + 65), relativePos + position, facingRotation);
+						chunks[i].SetBodyType(BodyType.Static);
+					}
+				} else
+				{
+                    chunks = new IObject[1];
+                    chunks[0] = Game.CreateObject("MetalDebris00A", position, (float)(rnd.NextDouble() * Math.PI * 2));
+                    chunks[0].SetBodyType(BodyType.Static);
                 }
+
+
+					Events.UpdateCallback delay = null;
+                delay = Events.UpdateCallback.Start(e => {
+					bool spellBroke = false;
+					foreach(IObject chunk in chunks)
+					{
+						if(chunk == null || chunk.DestructionInitiated || chunk.IsRemoved){
+							spellBroke = true;
+						}
+						if (spellBroke) chunk.SetBodyType(BodyType.Dynamic);
+						else chunk.Destroy();
+						messageRoss(spellBroke? "spellBroke" : "not spellBroke");
+					}
+					if (spellBroke){
+						messageRoss("stopping blast");
+						delay.Stop();
+						return;
+					}
+
+
+                    float bullets = splash * 1.5f;
+                    for (int i = 0; i < bullets; i++)
+                    {
+                        double rotation = rnd.NextDouble() * Math.PI * 2;
+                        Vector2 vector = new Vector2((float)Math.Cos(rotation) / 10f, (float)Math.Sin(rotation) / 10f);
+                        vector.Normalize();
+                        IProjectile shrapnel = Game.SpawnProjectile(ProjectileItem.PISTOL, position + vector * 10f, vector);
+                        shrapnel.DamageDealtModifier = (spellPower / 10f) / PISTOL_DAMAGE;
+                        shrapnel.CritChanceDealtModifier = 0f;
+                        //shrapnel.Velocity = vector;
+                    }
+
+                    delay.Stop();
+                }, 1300);
+                
             }
 
             public override void passive(Cast sender, IObject target, Vector2 vector)
@@ -77,11 +129,12 @@ namespace SFDScript
 			}
 
 
-
+			private IObject sawblade;
 			protected override void projectile(Vector2 position, Vector2 direction)
 			{
 				cast = new CastProjectile(position, direction + position, speed, this);
-				((CastProjectile)cast).attach(Game.CreateObject("Pulley00", position));
+				sawblade = Game.CreateObject("Pulley00", position);
+                ((CastProjectile)cast).attach(sawblade);
                 Game.PlaySound("Sawblade", position, 1f);
             }
             protected override void setUpStats()
