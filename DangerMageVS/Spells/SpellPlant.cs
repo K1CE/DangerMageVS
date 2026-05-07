a0e8ac9737c3a33f88cd5fecca789eb48f8e6009
K1CE <6kouman@gmail.com>
2026-05-06 22:51:58 -0700

    feat: plant tether

--- DangerMageVS/Spells/SpellPlant.cs
+++ DangerMageVS/Spells/SpellPlant.cs
@@ -21,24 +21,34 @@ namespace SFDScript
 			//TODO: add impact particle effects
 			public override void affect(Cast sender, IObject target, Vector2 vector, float powerMod)
 			{
-                float effectivePower = spellPower * powerMod;
+				float effectivePower = spellPower * powerMod;
+
+				for (int i = 0; i < splash / 2f; i++)
+				{
+					for(int j = 0; j < 5; j++)
+					{
+						Vector2 throwVec = new Vector2((float)Math.Cos(rnd.NextDouble() * Math.PI*2) * splash, 
+							(float)Math.Sin(rnd.NextDouble() * Math.PI * 2) * splash);
+						if( tieObject(target, throwVec, effectivePower)) break;
+						
+					}
+				}
 
 
-			}
+            }
 
 			public override void explode(Cast sender, IObject alreadyHit, Vector2 position) 
             {
 				base.explode(sender, alreadyHit, position);
 
 
-
             }
 
 			protected override void setUpStats()
 			{
-				spellPower = 14f;
+				spellPower = 13f;
 				cooldown = 3000;
-				speed = 7f;
+				speed = 6.7f;
 				range = 0.6f;
 				splash = 25;
 				particleEffect = elementEffects[(int)element];
@@ -55,6 +65,48 @@ namespace SFDScript
 				cast = new CastProjectile(position, direction + position, speed, this);
 				IObject leaf = Game.CreateObject("ItemDebrisFlamethrower00", position);
 				((CastProjectile)cast).attach(leaf);
+			}
+
+			private void createTimedTether(IObject target, Vector2 tieTo, IObject anchor, float power)
+			{
+				IObjectPullJoint tether = (IObjectPullJoint)Game.CreateObject("PullJoint", target.GetWorldPosition());
+				IObjectTargetObjectJoint targetJoint = (IObjectTargetObjectJoint)Game.CreateObject("TargetObjectJoint", tieTo);
+
+				targetJoint.SetTargetObject(anchor);
+
+				tether.SetTargetObject(target);
+				tether.SetTargetObjectJoint(targetJoint);
+
+				tether.SetLineVisual(LineVisual.DJVine);
+				tether.SetForcePerDistance(1f + power/17f);
+
+
+                Events.UpdateCallback despawn = null;
+                despawn = Events.UpdateCallback.Start(e => {
+
+					//Game.PlayEffect(elementEffects[(int)element], tether.GetWorldPosition() + (targetJoint.GetWorldPosition() - tether.GetWorldPosition())/2f);
+
+					tether.Remove();
+					targetJoint.Remove();
+
+                    despawn.Stop();
+                }, (uint)(100 * power));
+            }
+			private bool tieObject(IObject target, Vector2 shootAt, float power)
+			{
+				RayCastInput input = new RayCastInput();
+				input.AbsorbProjectile = RayCastFilterMode.True;
+				input.ClosestHitOnly = true;
+				input.IncludeOverlap = false;
+				RayCastResult[] results = Game.RayCast(target.GetWorldPosition(), shootAt, input);
+				//messageRoss("made it here!!!");
+				if (results.Length > 0 && results[0].Hit)
+                {
+                    createTimedTether(target, results[0].Position, results[0].HitObject, power);
+					return true;
+				}
+
+				return false;
 			}
 
 		}
